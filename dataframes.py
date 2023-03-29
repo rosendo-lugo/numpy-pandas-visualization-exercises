13933efdccd90c6e95254e731d0dff11f43ce462
Rosendo Lugo <rosendo.lugo@gmail.com>
2023-03-28 20:44:02 -0500

    Added a new file named advanced dataframes lesson review and modified the dataframes file

--- dataframes.py
+++ dataframes.py
@@ -146,9 +146,9 @@ mpg[mpg['class'] == 'compact']
 
 mpg_compact = mpg[mpg['class'] == 'compact']
 
-mpg_compact.hightway.min()
-mpg_compact[mpg_compact.highway == mpg_compact.hightway.min()]
-mpg_compact[mpg_compact.highway == mpg_compact.hightway.max()]
+# mpg_compact.hightway.min()
+mpg_compact[mpg_compact.highway == mpg_compact.highway.min()]
+mpg_compact[mpg_compact.highway == mpg_compact.highway.max()]
 
 # j. Create a column named average_mileage that is the mean of the city 
 # and highway mileage.
@@ -159,7 +159,7 @@ print(mpg.head())
 
 # k. Which dodge car has the best average mileage? The worst?
 print(f'\nPart II, Question k')
-mpg_dodge = mpg[mpg.manufacture == 'dodge']
+mpg_dodge = mpg[mpg.manufacturer == 'dodge']
 
 mpg_dodge.head()
 
@@ -167,28 +167,33 @@ mpg_dodge[mpg_dodge.avg_milage == mpg_dodge.avg_milage.min()]
 
 
 # 4. Load the Mammals dataset. Read the documentation for it, and use the data to answer these questions:
-data('Mamals', show_doc=True)
-mams = data('Mamals')
+print(f'\nPart III, Load the Mamals dataframe')
+
+data('Mammals', show_doc=True)
+
+mams = data('Mammals')
 
 
 # a. How many rows and columns are there?
+print(f'\nPart III, Question a')
 mams.shape
 
 # b. What are the data types?
+print(f'\nPart III, Question b')
 mams.head()
 
 # c. Summarize the dataframe with .info and .describe
-
+print(f'\nPart III, Question c')
 mams.info()
 mams.describe()
 # d. What is the the weight of the fastest animal?
-
+print(f'\nPart III, Question d')
 mams.speed.max()
 mams[mams.speed == mams.speed.max()]
 
 
 # e. What is the overal percentage of specials?
-
+print(f'\nPart III, Question e')
 mams.specials.sum()
 len(mams)
 (mams.specials.sum()/len(mams))*100
@@ -198,18 +203,26 @@ mams.specials.value_counts(True)
 
 
 # f. How many animals are hoppers that are above the median speed? 
+print(f'\nPart III, Question f')
+mams_columns = mams.columns
+print(mams_columns)
 
+mams[mams.hoppers]
+print('\n',mams[mams.hoppers])
 
-mams_hop = mams[mams.hoppers == True]
+mams_hop = [mams.speed > mams.speed.median()]
+print('\n',mams_hop)
 
-print(mams_hop)
+mams_hop_median = mams[(mams.hoppers == True) & (mams.speed > mams.speed.median())]
 
-mams_hop[mams.hop.speed >= mams.speed.median()]
+round(len(mams[(mams.hoppers == True) & (mams.speed > mams.speed.median())])/len(mams) * 100, 2)
+
+hoppers = mams[(mams.hoppers == True)]
+hoppers.head()
+print('\n',hoppers.head())
 
 # What percentage is this?
+print(f'\nPart III, Question f(a)')
+percentage = round(len(hoppers[(hoppers.speed > mams.speed.median())]) /len(mams) *100, 2)
 
-len(mams_hop[mams.hop.speed >= mams.speed.median()]/mams.shape[0]) * 100
-
-mams.shape[0]
-
-
+print(f'The percentage is', percentage)
