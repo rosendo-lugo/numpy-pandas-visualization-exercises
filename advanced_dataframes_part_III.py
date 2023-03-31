189797573e1e99ac9c3bb9415ba319e7a48b4fcf
Rosendo Lugo <rosendo.lugo@gmail.com>
2023-03-30 23:37:22 -0500

    Added the matplolib exercises and modified the advanced dataframes part III file

--- advanced_dataframes_part_III.py
+++ advanced_dataframes_part_III.py
@@ -13,8 +13,8 @@ from env import get_db_url # This allows you to find the get_db_url function fro
 # This retrives the orders table from the Chipotle database
 # If you need to change databases and tables just update "chipotle (database) and orders (table)"
 url = get_db_url('chipotle') # Chipotle is the database were the orders table is coming from. Don't forget that Chipotle is a database not a table.
-orders_table = pd.read_sql('SELECT * FROM orders', url) # Orders is a table. The table is located in the mySQL
-print('\n',orders_table)
+orders_table_df = pd.read_sql('SELECT * FROM orders', url) # Orders is a table. The table is located in the mySQL
+print('\n',orders_table_df)
 
 
 # 1. What is the total price for each order?
@@ -33,11 +33,11 @@ print(f'\n************** Option 1 **************\n',total_price_df)
 
 # ************** Option 2 **************
 # clean the item_price column
-orders_table['item_price'] = orders_table['item_price']\
+orders_table_df['item_price'] = orders_table_df['item_price']\
     .str.replace('$', '').astype(float)
 
 # pivot the data to get the total price for each order
-order_totals = pd.pivot_table(orders_table, values='item_price',\
+order_totals = pd.pivot_table(orders_table_df, values='item_price',\
                                index='order_id', aggfunc=np.sum)
 
 print(f'\n************** Option 2 **************\n',order_totals,'\n')
@@ -47,13 +47,13 @@ print(f'\n************** Option 2 **************\n',order_totals,'\n')
 # 2. What are the most popular 3 items?
 print('\nExercises III, Question 2')
 # ************** Option 1 **************
-popular_three = orders_table.groupby('item_name')['quantity'].sum()
+popular_three = orders_table_df.groupby('item_name')['quantity'].sum()
 top_three = popular_three.sort_values(ascending=False).head(3)
 print(f'\n************** Option 1 **************\n',top_three)
 
 
 # ************** Option 2 **************
-most_popular = pd.pivot_table(data=orders_table, index='item_name', values='quantity', aggfunc='sum')
+most_popular = pd.pivot_table(data=orders_table_df, index='item_name', values='quantity', aggfunc='sum')
 three_popular = most_popular.nlargest(3, 'quantity')
 print(f'\n************** Option 2 **************\n',three_popular) 
 
@@ -65,7 +65,7 @@ print('\n\nExercises III, Question 3')
 
 # Use the pivot_table() method to group orders by item_name and order_id
 #  and sum the quantity and item_price columns for each group
-order_totals = pd.pivot_table(orders_table, index='item_name',\
+order_totals = pd.pivot_table(orders_table_df, index='item_name',\
                                values=['quantity', 'item_price'], aggfunc=np.sum)
 
 
@@ -80,12 +80,28 @@ print(f'The item with the most Revenue\n\n',most_revenue)
 
 # 4. Join the employees and titles DataFrames together.
 print('\nExercises III, Question 4')
+url = get_db_url('employees') # Employees is the database were the orders table is coming from.
+# Don't forget that Employee is a database not a table.
+titles_table_df = pd.read_sql('SELECT * FROM titles', url) # Title is a table. The table is located in the mySQL and was limited to 1000 rows
+employees_table_df = pd.read_sql('SELECT * FROM employees', url) # Employees is a table. The table is located in the mySQLL and was limited to 1000 rows
+print('\n',titles_table_df.head())
+print('\n',employees_table_df.head())
 
+et_tt_merge_df = pd.merge(titles_table_df, employees_table_df, how='', on='emp_no')
+print('\n',et_tt_merge_df)
 
 # 5. For each title, find the hire date of the employee
 # that was hired most recently with that title.
 print('\nExercises III, Question 5')
 
+# Examples of sorting and groupby
+# format: pd.merge(df1,df2, how='', on='column_name')
+# df.sort_values(by='col1', ascending=False)
+# format: df.groupby('column_name').agg_function()
+# df.groupby('room').max()[['math','reading','english']]
+
+
+# titles_table_df.groupby('title').sort()[['from_date']]
 
 # 6. Write the code necessary to create a cross tabulation
 # of the number of titles by department. (Hint: this 
---
+++ matplotlib_exercises.py
@@ -0,0 +1,68 @@
+# Exercises
+# Using the repo setup directions, setup a new local and remote repository
+    # named numpy-pandas-visualization-exercises. The local version of your
+    # repo should live inside of ~/codeup-data-science. This repo should be
+    # named numpy-pandas-visualization-exercises
+
+# Do your work for this exercise in a jupyter notebook named 
+    # matplotlib_exercises.
+import matplotlib.pyplot as plt
+
+
+# 1. Use matplotlib to plot the following equation:
+
+    # y = x^2 − x + 2
+    # You'll need to write the code that generates the x and y points.
+
+    # Add an anotation for the point 0, 0, the origin.
+print('\nQuestion 1')
+
+
+# 2. Create and label 4 separate charts for the following equations
+    # (choose a range for x that makes sense):
+
+    # y = √ x
+    # y = x^3
+    # y = 2^x
+    # y = 1 / (x + 1)
+
+    # You can use functions from the math module to help implement
+    #  some of the equations above.
+print('\nQuestion 2')
+
+
+# 3. Combine the figures you created in the last step into one large
+    # figure with 4 subplots.
+print('\nQuestion 3')
+
+
+# 4. Combine the figures you created in the last step into one figure
+    # where each of the 4 equations has a different color for the points.
+    # Be sure to include a legend and an appropriate title for the figure.
+print('\nQuestion 4')
+
+
+# 5. Make a new Jupyter notebook named big_o_notation.ipynb
+
+    # Title your chart "Big O Notation"
+    # Label your x axis "Elements"
+    # Label your y axis "Operations"
+    # Label your curves or make a legend for the curves
+    # Use LaTex notation where possible
+    # Curves to graph
+
+    # y = 0n + 1  and label the curve "O(1)"
+    # y = log (n)  and label the curve "O(log n)"
+    # y = n  and label the curve "O(n)"
+    # y = n ∗ log (n) and label it "O(n log n)"
+    # y = n^2 and label it "O(n^2)"
+    # y = 2^n  and label it "O(2^n)"
+    # y = n^!  and label it "O(n!)"
+    # y = n^n  and label it "O(n^n)"
+print('\nQuestion 5')
+
+
+
+
+
+
